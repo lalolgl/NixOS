c2e0a3fd9edd4f42fb1d25cc8bceb61671ec9064
AOG <flava.hustle.hard@gmail.com>
2026-04-14 13:06:44 +0300

    Update gnome.nix

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -7,4 +7,5 @@
   # Enable the GNOME Desktop Environment
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+  services.displayManager.sessionPackages = [ pkgs.niri  ];
 }
