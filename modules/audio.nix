7e81aef8976522aa6e09789d1178d9cf7c675844
AOG <flava.hustle.hard@gmail.com>
2026-04-14 13:06:36 +0300

    Added a new PipeWire config

--- modules/audio.nix
+++ modules/audio.nix
@@ -11,5 +11,32 @@
       pulse.enable = true;
       jack.enable = true;
       #media-session.enable = true;
+      extraConfig.pipewire = {
+        "10-clock-rate" = {
+          "context.properties" = {
+            "default.clock.allowed-rates" = [
+              # 44100
+              48000
+              96000
+              # 384000
+              # 768000
+            ];
+            "default.clock.rate" = 96000;
+            "settings.check-rate" = true;
+          };
+        };
+        "09-no-resample" = {
+          "context.properties" = {
+            "resample.disable" = true;
+          };
+        };
+        "11-latency" = {
+          "context.properties" = {
+            "default.clock.quantum" = 256;
+            "default.clock.min-quantum" = 128;
+            "default.clock.max-quantum" = 512;
+          };
+        };
+      };
     };
 }
