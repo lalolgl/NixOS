6b17a2a74eb5206f2024b163977c9b9b4b2fa4d8
AOG <flava.hustle.hard@gmail.com>
2026-07-04 12:18:59 +0300

    Update packages.nix

--- modules/packages.nix
+++ modules/packages.nix
@@ -14,6 +14,7 @@
     # VPN
     xray
     tun2proxy
+    v2ray
 
     # Security
     #opensnitch
@@ -76,6 +77,7 @@
     upscayl
     ollama-cuda
     #openclaw
+    opencode
 
     # Office
     onlyoffice-desktopeditors
