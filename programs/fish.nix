92beae52eea62dfb555fa89162d7b0c1f57529fa
AOG <flava.hustle.hard@gmail.com>
2026-07-27 22:40:10 +0300

    Update fish.nix

--- programs/fish.nix
+++ programs/fish.nix
@@ -7,6 +7,7 @@
     shellAliases = {
       ll = "ls -l";
       update = "cd \"$HOME/.nixos\" && sudo nixos-rebuild switch --flake .";
+      clean = "cd \"$HOME/.nixos\" && sudo nix-collect-garbage -d";
     };
 
     interactiveShellInit = ''
